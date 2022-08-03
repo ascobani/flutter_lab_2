0586b6982e04c53a672e92edce1c5fd5277fafa7
alisinancobani <cobanialisinan@gmail.com>
2022-08-03 17:12:35 +0300

    colors

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,74 +10,71 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return SizedBox(
-      height: 600,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map(
-            (transaction) {
-              /* 
-                  getColor() {
-                    if (transaction.amount >= 1000) {
-                      return Colors.redAccent;
-                    } else if (transaction.amount >= 750) {
-                      return Colors.redAccent;
-                    } else if (transaction.amount >= 500) {
-                      return Colors.yellow;
-                    } else if (transaction.amount >= 250) {
-                      return Colors.greenAccent;
-                    } else if (transaction.amount >= 0) {
-                      return Colors.green;
-                    }
-                  }
-                  */
-              return Card(
-                child: Row(
+      height: 554,
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+           getColor() {
+              if (transactions[index].amount >= 1000) {
+                return Colors.redAccent;
+              } else if (transactions[index].amount >= 750) {
+                return Colors.redAccent;
+              } else if (transactions[index].amount >= 500) {
+                return Colors.yellow;
+              } else if (transactions[index].amount >= 250) {
+                return Colors.greenAccent;
+              } else if (transactions[index].amount >= 0) {
+                return Colors.green;
+              }else{
+                return Colors.black;
+              }
+            }
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: getColor(), //can't figure it out
+                      //color: Colors.black(),
+                      width: 2,
+                    ),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    '₺${transactions[index].amount}',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w200,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          //color: getColor(), //can't figure it out
-                          color: Colors.black,
-                          width: 2,
-                        ),
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        '₺${transaction.amount}',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w200,
-                          fontSize: 20,
-                        ),
+                    Text(
+                      transactions[index].title,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          transaction.title,
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          DateFormat.yMd().format(transaction.date),
-                          style:
-                              const TextStyle(color: Colors.grey, fontSize: 13),
-                        ),
-                      ],
+                    Text(
+                      DateFormat.yMd().format(transactions[index].date),
+                      style: const TextStyle(color: Colors.grey, fontSize: 13),
                     ),
                   ],
                 ),
-              );
-            },
-          ).toList(),
-        ),
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
       ),
     );
   }
